6457a0c53896c2f3a8b9c199099944509e3fc6d6
Mahmoud <mahmoud.ahmed12@gmail.com>
2026-02-16 23:38:13 +0200

    upload

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+export const content = ["./**/*.{html,js}"];
+export const theme = {
+  extend: {
+    fontFamily: {
+      opensans: ['"Open Sans"', "sans-serif"],
+    },
+  },
+};
+export const plugins = [];
